98a39efd35674b2c1b4b878361bcf16a09ad0579
CT <2208666380@qq.com>
2021-12-12 17:36:49 -0800

    ejs is basically success.

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose');
+const ejs = require('ejs')
 mongoose.connect('mongodb://172.21.2.236:27017/190110910405');
 const schema = {
     name:String,
@@ -14,9 +15,15 @@ const mydata = mongoose.model('cat1s', schema);
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    res.send(req.query)
+    // res.send(req.query)
     console.log(req.query)
     const kitty = new mydata({ name: req.query.first,health:req.query.second });
     kitty.save()
+    // ejs.renderFile(filename, data, options, function(err, str){
+    //     // str => 输出渲染后的 HTML 字符串
+    // });
+    ejs.renderFile("result.html",{returnValue:"success"},(ree,str)=>{
+        res.send(str)
+    })
 })
 app.listen(60405)
